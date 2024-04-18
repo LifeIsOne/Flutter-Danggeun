116463a2e60f24a107a59d9f8565f4daa46c448e
LifeIsOne <didtmdgh258@gmail.com>
2024-04-18 16:41:03 +0900

    테마 적용

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:daangn/screens/main_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,15 +12,41 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        title: 'carrot_market_ui',
-        home: MainScreen(),
-        theme: ThemeData(
-          scaffoldBackgroundColor: Colors.yellow,
-          textTheme: TextTheme(),
-          appBarTheme: AppBarTheme(),
-          bottomNavigationBarTheme: BottomNavigationBarThemeData(),
-          primarySwatch: Colors.orange,
-        ));
+      debugShowCheckedModeBanner: false,
+      title: 'carrot_market_ui',
+      home: MainScreen(),
+      theme: ThemeData(
+        scaffoldBackgroundColor: Colors.yellow,
+        textTheme: TextTheme(
+          displayLarge:
+              GoogleFonts.openSans(fontSize: 18.0, color: Colors.black),
+          displayMedium: GoogleFonts.openSans(
+              fontSize: 16.0, color: Colors.black, fontWeight: FontWeight.bold),
+          bodyLarge: GoogleFonts.openSans(fontSize: 16.0, color: Colors.black),
+          bodyMedium: GoogleFonts.openSans(fontSize: 14.0, color: Colors.black),
+          titleMedium:
+              GoogleFonts.openSans(fontSize: 15.0, color: Colors.black),
+        ),
+        appBarTheme: AppBarTheme(
+          centerTitle: false,
+          color: Colors.white,
+          elevation: 0.0,
+          iconTheme: IconThemeData(
+            color: Colors.black,
+          ),
+          titleTextStyle: GoogleFonts.nanumGothic(
+            fontSize: 16,
+            fontWeight: FontWeight.bold,
+            color: Colors.black,
+          ),
+        ),
+        bottomNavigationBarTheme: BottomNavigationBarThemeData(
+          selectedItemColor: Colors.orange,
+          unselectedItemColor: Colors.black54,
+          showUnselectedLabels: true,
+        ),
+        primarySwatch: Colors.orange,
+      ),
+    );
   }
 }
